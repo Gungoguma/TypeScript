55345a162dbe6cd7137598c80fe7b89d001ecfd3
Gungoguma <khh9550@naver.com>
2021-05-21 23:09:35 +0900

    React: CSS in JS, React Hooks

--- sinamom_react/src/App.tsx
+++ sinamom_react/src/App.tsx
@@ -1,10 +1,26 @@
-import React, { } from 'react';
+import React, { useEffect, useState } from 'react';
 import Text from './Text';
 
 const App: React.FC = () => {
+    const [num, setNum] = useState(1);
+
+
+    const addNum = () => {
+        setNum((prevnum) => prevnum + 1);
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setNum((prevnum) => prevnum - 1);
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        }
+    }, []);
     return (
         <>
-            <Text color="blue">A</Text>
+            <Text color="blue" onClick={addNum}>{num}</Text>
         </>
     );
 }
--- sinamom_react/src/Text.tsx
+++ sinamom_react/src/Text.tsx
@@ -1,19 +1,27 @@
 import React, { } from 'react';
+import styled from 'styled-components';
 
 interface TextProps {
     readonly color: string;
+    readonly onClick?: React.MouseEventHandler;
 }
 
-const Text: React.FC<TextProps> = ({ children, color }) => {
+const ColorText = styled.p< { a: string } >`
+    font-weight : bold;
+    color : ${(props) => props.a};
+    user-select : none;
+
+    &:hover {
+        font-weight : normal;
+    }
+`;
+
+const Text: React.FC<TextProps> = ({ children, color, onClick }) => {
     return (
         <>
-            <p style={{
-                color: color, //Key, Value 같아서 생략가능
-                fontWeight: "bold"
-            }}
-            >
-                {children}
-            </p>
+            <ColorText a={color} onClick={onClick}>
+                글자 : {children}
+            </ColorText>
         </>
     );
 }
