692540133c9a9229ef3f30e2fd8dc6c3ac2d7f06
Gungoguma <khh9550@naver.com>
2021-05-29 23:06:16 +0900

    cookie, session

---
+++ express/21_05_29/src/index.ts
@@ -0,0 +1,22 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import session from 'express-session';
+import DataApi from "./apis/data";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : false })); //해석 라이브러리 내장 : flase 외장 : true
+app.use(cookieParser());
+app.use(session({
+    secret: 'asdasdasd',
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(DataApi);
+
+
+//서버 시작 숫자는 포트번호(보통 8080) 코드 구현하고 쓰기
+app.listen(8080, () => { //성공하면 콜백
+    console.log("Start server");
+});
